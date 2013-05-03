ae4a0dd1f2d7f15c1f327b1d4d94570d8fe855a4
Jacob Kelley <jakie8@gmail.com>
2013-05-03 16:37:37 -0700

    html -> PHP

--- includes/drawers.php
+++ includes/drawers.php
@@ -1,11 +1,11 @@
 		<div class="snap-drawer snap-drawer-left" id="left-drawer">
 			<ul class="list">
 				<li class="list-divider">You</li>
-				<li><a href="profile.html"><img src="static/img/placeholders/50.gif" class="avatar"> jakiestfu</a></li>
+				<li><a href="profile.php"><img src="static/img/placeholders/50.gif" class="avatar"> jakiestfu</a></li>
 				<li class="list-divider">Templates</li>
-				<li><a href="explore.html">Explore</a></li>
-				<li><a href="feed.html">Feed</a></li>
-				<li><a href="settings.html">Settings</a></li>
+				<li><a href="explore.php">Explore</a></li>
+				<li><a href="feed.php">Feed</a></li>
+				<li><a href="settings.php">Settings</a></li>
 				<li class="list-divider">Filler List</li>
 				<li>List item 1 <span class="chevron"></span></li>
 				<li>List item 2 <span class="chevron"></span></li>
